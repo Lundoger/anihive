53699fcd2a73df80275826488f5748fd77cf8fee
Богдан <sysoevbogdan82@gmail.com>
2025-12-16 00:06:00 +0200

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,12 @@
 import { getRequestConfig } from "next-intl/server";
-import { cookies } from "next/headers";
+import { routing } from "./routing";
 
-export default getRequestConfig(async () => {
-  const store = await cookies();
-  const locale = store.get("locale")?.value || "ua";
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
+
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
 
   return {
     locale,
