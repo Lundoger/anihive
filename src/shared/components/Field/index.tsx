bc2cd5d248d033b3fca8d613bac20cdc853da2fe
Sysoev Bogdan <sysoevbogdan82@gmail.com>
2026-08-19 18:32:37 +0300

    fix: autofilled inputs are detecting value

--- src/shared/components/Field/index.tsx
+++ src/shared/components/Field/index.tsx
@@ -118,7 +118,7 @@ function FieldLabel({
         "text-gray basic-transition pointer-events-none absolute top-1/2 z-10 inline-block w-fit! -translate-y-1/2 bg-black px-1 text-base duration-100",
         "left-3 group-has-data-[slot=input-prefix]/field:left-10",
         "group-focus-within/field:text-primary-accent-light group-focus-within/field:top-0 group-focus-within/field:left-2! group-focus-within/field:-translate-y-1/2 group-focus-within/field:text-sm group-focus-within/field:font-normal",
-        "leading-none group-has-[[data-slot=input]:not(:placeholder-shown)]/field:top-0 group-has-[[data-slot=input]:not(:placeholder-shown)]/field:left-2 group-has-[[data-slot=input]:not(:placeholder-shown)]/field:-translate-y-1/2 group-has-[[data-slot=input]:not(:placeholder-shown)]/field:text-sm group-has-[[data-slot=input]:not(:placeholder-shown)]/field:font-normal",
+        "leading-none group-has-[[data-slot=input]:is(:not(:placeholder-shown),:-webkit-autofill,:autofill)]/field:top-0 group-has-[[data-slot=input]:is(:not(:placeholder-shown),:-webkit-autofill,:autofill)]/field:left-2 group-has-[[data-slot=input]:is(:not(:placeholder-shown),:-webkit-autofill,:autofill)]/field:-translate-y-1/2 group-has-[[data-slot=input]:is(:not(:placeholder-shown),:-webkit-autofill,:autofill)]/field:text-sm group-has-[[data-slot=input]:is(:not(:placeholder-shown),:-webkit-autofill,:autofill)]/field:font-normal",
         "group/field-label peer/field-label flex w-fit gap-2 group-data-[disabled=true]/field:opacity-50",
         "has-[>[data-slot=field]]:flex-col has-[>[data-slot=field]]:rounded-md has-[>[data-slot=field]]:border *:data-[slot=field]:p-4",
         "has-data-[state=checked]:bg-primary/5 has-data-[state=checked]:border-primary dark:has-data-[state=checked]:bg-primary/10",
